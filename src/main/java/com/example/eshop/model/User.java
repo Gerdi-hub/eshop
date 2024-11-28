e3bf3dd63afd54a1234a382c73b228df707ad102
Karin <karinhenn89@gmail.com>
2024-11-28 13:05:25 +0200

    project start

--- src/main/java/com/example/eshop/model/User.java
+++ src/main/java/com/example/eshop/model/User.java
@@ -1,4 +1,26 @@
 package com.example.eshop.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor              //klassi annotatsioonid
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+@ToString
+
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int userType;
 }
