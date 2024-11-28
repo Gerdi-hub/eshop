ee035924ba37c43c9eb17f9c37fdbbd9ff237a21
Karin <karinhenn89@gmail.com>
2024-11-28 14:51:11 +0200

    added classes, repos, tables

--- src/main/java/com/example/eshop/repository/ProductRepository.java
+++ src/main/java/com/example/eshop/repository/ProductRepository.java
@@ -1,4 +1,7 @@
 package com.example.eshop.repository;
 
-public interface ProductRepository {
+import com.example.eshop.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
