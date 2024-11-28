faafb4c02c6e428498a607fd5652ba19bf77833a
Gerdi <orawakene@gmail.com>
2024-11-28 14:52:02 +0200

    UserService, validateUser

--- src/main/java/com/example/eshop/service/UserService.java
+++ src/main/java/com/example/eshop/service/UserService.java
@@ -1,4 +1,33 @@
 package com.example.eshop.service;
 
+import com.example.eshop.model.User;
+import com.example.eshop.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
 public class UserService {
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public boolean validateUser(String username, String password) {
+        List<User> userList = userRepository.findAll();
+        boolean validUser = false;
+        for (User user : userList) {
+            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                validUser = true;
+
+            }
+        }
+        return validUser;
+    }
+
+
+
 }
