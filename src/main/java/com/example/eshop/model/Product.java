155973724e71caaea38f6396dbafd0a6eb63c9de
Karin <karinhenn89@gmail.com>
2024-11-28 13:05:25 +0200

    added classes:user, product, repo

--- src/main/java/com/example/eshop/model/Product.java
+++ src/main/java/com/example/eshop/model/Product.java
@@ -1,4 +1,27 @@
 package com.example.eshop.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@NoArgsConstructor              //klassi annotatsioonid
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "")
+@ToString
+
 public class Product {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+
 }
--- src/main/java/com/example/eshop/repository/UserRepository.java
+++ src/main/java/com/example/eshop/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.example.eshop.repository;
 
-public interface UserRepository {
+import com.example.eshop.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
