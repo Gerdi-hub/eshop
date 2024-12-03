9c532eff1900e03fdde04f6208bec8f0189632be
Karin <karinhenn89@gmail.com>
2024-12-03 09:05:57 +0200

    added AdminController and AdminService meetodid ja mapping

--- src/main/java/com/example/eshop/service/ShoppingCartService.java
+++ src/main/java/com/example/eshop/service/ShoppingCartService.java
@@ -1,4 +1,28 @@
 package com.example.eshop.service;
 
+import com.example.eshop.model.ShoppingCart;
+import com.example.eshop.repository.ShoppingCartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class ShoppingCartService {
+
+    @Autowired
+    private final ShoppingCartRepository shoppingCartRepository;
+
+    public ShoppingCartService(ShoppingCartRepository shoppingCartRepository) {
+        this.shoppingCartRepository = shoppingCartRepository;
+
+    }
+
+    public ShoppingCart addProductToShoppingCart(ShoppingCart shoppingCart) {
+        return shoppingCartRepository.save(shoppingCart);
+    }
+
+    public List<ShoppingCart> getAllCartItems(){
+        return shoppingCartRepository.findAll();
+    }
+
+
 }
