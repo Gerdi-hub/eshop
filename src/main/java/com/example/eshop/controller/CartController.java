4d3836fc5b1562237a169838874990e79697261f
Karin <karinhenn89@gmail.com>
2024-12-03 10:14:17 +0200

    katse 2

--- src/main/java/com/example/eshop/controller/CartController.java
+++ src/main/java/com/example/eshop/controller/CartController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "*")
 public class CartController {
 
-    @Autowired
     private final ShoppingCartService shoppingCartService;
 
 
--- src/main/java/com/example/eshop/service/ShoppingCartService.java
+++ src/main/java/com/example/eshop/service/ShoppingCartService.java
@@ -3,26 +3,26 @@ package com.example.eshop.service;
 import com.example.eshop.model.ShoppingCart;
 import com.example.eshop.repository.ShoppingCartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ShoppingCartService {
 
-    @Autowired
     private final ShoppingCartRepository shoppingCartRepository;
 
     public ShoppingCartService(ShoppingCartRepository shoppingCartRepository) {
         this.shoppingCartRepository = shoppingCartRepository;
-
     }
 
     public ShoppingCart addProductToShoppingCart(ShoppingCart shoppingCart) {
         return shoppingCartRepository.save(shoppingCart);
     }
 
-    public List<ShoppingCart> getAllCartItems(){
+  /*  public List<ShoppingCart> getAllCartItems(){
         return shoppingCartRepository.findAll();
-    }
+    }*/
 
 
 }
